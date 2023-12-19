327cfd6fb75bb604680214c0abb8b2b8c428a022
Viktoriya Hristova <vihristo@progress.com>
2023-12-19 13:10:49 +0200

    Implement upload-file logic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using MyMOVEItTask.Models;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace MyMOVEItTask.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebHostEnvironment hostingEnviroment;
         private readonly ILogger<HomeController> _logger;
+        static HttpClient client = new HttpClient();
+        static string hardCodedDirId = "115502127";
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IWebHostEnvironment _hostingEnviroment, ILogger<HomeController> logger)
         {
+            hostingEnviroment = _hostingEnviroment;
             _logger = logger;
         }
 
@@ -20,9 +25,50 @@ namespace MyMOVEItTask.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(FileUploadViewModel model)
+        public async Task<IActionResult> Index(FileUploadViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                if (model.File != null)
+                {
+                    var url = $"https://mobile-1.moveitcloud.com/api/v1/folders/{hardCodedDirId}/files";
+
+                    var filePath = Path.GetTempFileName();
+
+                    using var stream = new FileStream(filePath, FileMode.Create);
+                    await model.File.CopyToAsync(stream);
+
+                    using var fileContent = new StreamContent(stream);
+                    fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
+
+                    using var formPayload = new MultipartFormDataContent
+                    {
+                        { fileContent, "file", Path.GetFileName(filePath) }
+                    };
+
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", model.AccessToken);
+                    var response = await client.PostAsync(url, formPayload).ConfigureAwait(false);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                        ViewBag.SuccessMessage = "File uploaded successfully.";
+                    else
+                        ModelState.AddModelError("", "Failed to upload file.");
+                }
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Something went wrong.");
+
+                return View(model);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
